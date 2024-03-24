e07b927f51d0682833a28ed6aa202e361b65652a
rx-ted <1552488649@qq.com>
2024-03-24 10:46:09 +0800

    del lcd 8080 3.2 r61581

--- projects/1lcd_8080_3.2_r61581/upload.sh
+++
@@ -1,6 +0,0 @@
-scons -j16
-PROJECT_NAME=$(echo $(pwd) | tr "/" "\n" | tail -1)
-# stm32_tooler <-> STM32_Programmer_CLI   
-stm32_tooler -c port=SWD -el board/stldr/ART-Pi_W25Q64.stldr  -d output/$PROJECT_NAME.elf
-
-#About pyocd openocd, I took a try to get failuar...
